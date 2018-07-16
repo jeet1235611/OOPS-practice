f5da27e4af08d939b86b093d3e8d465b5b4f1371
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-17 03:09:45 +0530

    Add files via upload

---
+++ student1.cpp
@@ -0,0 +1,13 @@
+class Student{
+	public:
+	int age;
+
+	private:
+	int rollNumber;
+
+	public:
+	void display() {
+		cout << "Age = " << age << endl;
+		cout << "Roll Number = " << rollNumber << endl;
+	}
+};
---
+++ student1Use.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "student1.cpp"
+
+int main() {
+	Student s1, s2;
+	Student s3;
+
+	s1.display();
+	s2.display();
+	s3.display();
+}
