d65294bdc9d2eb0649615eb5b3baf05067f5dc6c
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-17 08:28:07 +0530

    Add files via upload

---
+++ constructorClass.cpp
@@ -0,0 +1,9 @@
+class Student{
+	public:
+		int age;
+		int rollNo;
+
+		Student(){
+			cout << "Hello world! " << endl;
+		}
+};
---
+++ constructorClassUse.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+#include "constructorClass.cpp"
+
+int main() {
+	Student s1, s2, s3, s4, s5;
+}
