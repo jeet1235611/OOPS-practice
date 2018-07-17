fc9d856246bd4b4fc4d3b470a91501877dc91a15
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-17 08:45:46 +0530

    Add files via upload

--- constructorClass.cpp
+++ constructorClass.cpp
@@ -2,8 +2,16 @@ class Student{
 	public:
 		int age;
 		int rollNo;
-
+		
 		Student(){
 			cout << "Hello world! " << endl;
+		}
+		
+		Student(int r) {
+		//	cout << "Parameterised constructor called!" << endl;
+		rollNo = r;
+		}
+		void display() {
+			cout << age << " " << rollNo << endl;
 		}
 };
--- constructorClassUse.cpp
+++ constructorClassUse.cpp
@@ -4,5 +4,18 @@ using namespace std;
 #include "constructorClass.cpp"
 
 int main() {
-	Student s1, s2, s3, s4, s5;
+	Student s1, s2, s3;
+	Student *s6 = new Student;
+	Student *s7 = new Student;
+	s1.display();
+	s2.display();
+	s3.display();
+
+	s6 -> display();
+	s7 -> display();
+
+	Student s10(99);
+
+	s10.display();
+
 }
