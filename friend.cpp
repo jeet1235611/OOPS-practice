610b9f2dffc1c04dde7b004d1f19715d5c8a3046
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-24 05:26:48 +0530

    Add files via upload

---
+++ friend.cpp
@@ -0,0 +1,21 @@
+class Human {
+	string name;
+	int age;
+
+	public:
+
+	Human(string iname, int iage) {
+		name = iname;
+		age = iage;
+	}
+
+	void tellme() {
+		cout << name << endl << age << endl;
+	}
+
+	friend void display(Human man);
+};
+
+void display(Human man) {
+	cout << man.name << endl << man.age << endl;
+}
---
+++ friendUse.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "friend.cpp"
+
+int main() {
+	Human anil("anil", 24);
+	display(anil);
+	return 0;
+}
