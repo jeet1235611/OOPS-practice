3436adb6b533620066db2968e0dff8aa2694d35e
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-24 12:10:12 +0530

    Add files via upload

---
+++ class9.cpp
@@ -0,0 +1,10 @@
+class Student {
+	public:
+		int rollNo;
+		int marks;
+	public:
+		Student() {
+			rollNo = 99;
+			marks = 99;
+		}
+};
---
+++ class9Use.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+#include "class9.cpp"
+int main() {
+	const Student s1;
+}
