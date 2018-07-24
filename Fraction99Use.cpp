38e084e740fbcda5511b6d32a8a5dc3037a0dbe1
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-24 06:44:05 +0530

    Add files via upload

--- Fraction99Use.cpp
+++ Fraction99Use.cpp
@@ -7,9 +7,15 @@ int main() {
 	Fraction f1(10, 2);
 	Fraction f2(15, 4);
 	
-	f1.print();
+/*	f1.print();
 	++(++f1);
 	f1.print();
+	*/
+
+	f1.print();
+	Fraction f3 = ++(++f1);
+	f1.print();
+	f3.print();
 
 	/*
 	f1.print();
