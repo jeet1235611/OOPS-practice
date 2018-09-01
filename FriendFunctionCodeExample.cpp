56708d1843d21c98ec80e96162b5456d4d2d655d
acoustic_jeet <js.jitendra11@gmail.com>
2018-09-02 03:56:02 +0530

    Add files via upload

--- FriendFunctionCodeExample.cpp
+++ FriendFunctionCodeExample.cpp
@@ -6,6 +6,8 @@ class Bus {
 		void print();
 };
 
+void test();
+
 class Truck {
 	private:
 		int x;
@@ -17,6 +19,8 @@ class Truck {
 		int z;
 
 	friend void Bus :: print();
+
+	friend void test();
 };
 
 void Bus :: print() {
@@ -26,7 +30,18 @@ void Bus :: print() {
 	cout << t.x << " " << t.y << endl;
 }
 
+void test() {
+	//Access truck private.
+	Truck t;
+	t.x = 10;
+	t.y = 20;
+	cout << t.x << " " << t.y << endl;
+}
+
 int main() {
+
 	Bus b;
 	b.print();
+	test();
+
 }
